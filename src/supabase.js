ecea68da7a3da5c85005b8ce21d040bfe79ffa5f
kjugi2 <karlo.jugec@gmail.com>
2025-08-29 22:34:26 +0200

    opis promjena

--- src/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// koristi varijable iz .env.local
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dvd-akt/',   // ⬅️ ime tvog GitHub repo-a
   plugins: [react()],
 })
